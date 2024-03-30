db6545318c2343608f837ead934a656f42081cee
aribaza <aribazaga@gmail.com>
2024-03-30 16:42:07 +0000

    added function to generate text for map stats box

--- helpers.py
+++ helpers.py
@@ -72,4 +72,66 @@ def token_required(f):
 
         return f(*args, **kwargs)
     return decorator
- 
+
+def trends_box(sexChoice, formatted_date, selected_layer):
+    formatted_date = int(formatted_date.split('-')[0])
+    comparison_date = formatted_date - 1
+
+    if selected_layer == 'rgn':
+        geocolumn = "Region"
+        data = pd.read_csv('Datasets/Edited_Forecasts/combined_forecast_Region_sex_age_group.csv')
+    elif selected_layer == 'lad':
+        geocolumn = "LAD"
+        data = pd.read_csv('Datasets/Edited_Forecasts/combined_forecast_LAD_sex_age_group.csv')
+    elif selected_layer == 'itl':
+        geocolumn = "ITL"
+        data = pd.read_csv('Datasets/Edited_Forecasts/combined_forecast_ITL_sex_age_group.csv')
+
+    # First year:
+    fdata = data[data['sex'] == sexChoice]
+    fdata = fdata[fdata['extract_date'].str.contains(str(formatted_date))]
+    if (selected_layer == 'lad' or selected_layer == 'itl') and filter == 1:
+        fdata = fdata[fdata['Region'] == 'London']
+        if selected_layer == 'lad':
+            fdata = fdata[fdata['ITL'] == fdata['ITL']]
+
+    # Group by 'Region' and sum the age columns
+    fdata = fdata.groupby(geocolumn).sum()
+
+    # Reset the index to make 'Region' a column again
+    fdata.reset_index(inplace=True)
+    age_columns = [col for col in fdata.columns if col.startswith('age_')]
+    fdata[f'{formatted_date}total_age'] = fdata[age_columns].sum(axis=1).astype(float)
+
+    # Reference year:
+    cdata = data[data['sex'] == sexChoice]
+    cdata = cdata[cdata['extract_date'].str.contains(str(comparison_date))]
+    if (selected_layer == 'lad' or selected_layer == 'itl') and filter == 1:
+        cdata = cdata[cdata['Region'] == 'London']
+        if selected_layer == 'lad':
+            cdata = cdata[cdata['ITL'] == cdata['ITL']]
+
+    # Group by 'Region' and sum the age columns
+    cdata = cdata.groupby(geocolumn).sum()
+
+    # Reset the index to make 'Region' a column again
+    cdata.reset_index(inplace=True)
+    age_columns = [col for col in cdata.columns if col.startswith('age_')]
+    cdata[f'{comparison_date}total_age'] = cdata[age_columns].sum(axis=1).astype(float)
+
+    # Merge the two dataframes on 'Region'
+    fdata = fdata.merge(cdata, on=geocolumn, how='left')
+
+    # Calculate the percentage change and add it as a new column
+    fdata['change'] = ((fdata[f'{formatted_date}total_age'] - fdata[f'{comparison_date}total_age']) / fdata[f'{comparison_date}total_age']) * 100
+
+    # Round the 'change' values to 2 significant figures
+    fdata['change'] = fdata['change'].round(2)
+
+    # Sort by 'change' column in descending order
+    fdata.sort_values('change', ascending=False, inplace=True)
+
+    # Convert to nested list
+    fdata = fdata.head(8)[[geocolumn, 'change']].values.tolist()
+
+    return fdata
